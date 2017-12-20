135e9b03cb78efa8e8b7456e1be592ffc7396897
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-20 19:45:03 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
-# Write your code here.
+def dictionary
+  dictionary_hash={
+    "hello"=>"hi",
+    "to"=>"2",
+    "two"=>"2",
+    "too"=>"2",
+    "for"=>"4",
+    "four"=>"4",
+    "be"=>"b",
+    "you"=>"u",
+    "at"=>"@",
+    "and"=>"&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.collect do |entry|
+    if dictionary.keys.include?(entry.downcase)
+      entry=dictionary[entry.downcase]
+    else
+      entry
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if 140<tweet.length
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet2=word_substituter(tweet)
+  if tweet2.length>140
+    tweet2[0...140]
+  else
+    tweet2
+  end
+end
